1f8bda33c8f3c67a87ba0fa9f14862a1d54b6164
Jonas Gierer <jonas@gierer.xyz>
2024-12-27 14:59:48 +0100

    Enable catppuccin nvim plugin integrations

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,14 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					blink_cmp = true,
+					snacks = true,
+					which_key = true,
+				},
+			})
+
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
