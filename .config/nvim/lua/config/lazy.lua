3dccc2afa769c265fc4b333e5deb39b62aa09376
Jonas Gierer <jonas@gierer.xyz>
2024-12-27 14:59:17 +0100

    Disable "Config Change Detected" message from lazy.nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
+			{ out,                            "WarningMsg" },
 			{ "\nPress any key to exit..." },
 		}, true, {})
 		vim.fn.getchar()
@@ -29,4 +29,6 @@ require("lazy").setup({
 	},
 	-- automatically check for plugin updates
 	checker = { enabled = true, notify = false },
+	-- disable "Config Change Dected" message
+	change_detection = { notify = false },
 })
