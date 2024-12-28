478dab4a2742106bf39edc889c22baf725d679b7
Jonas Gierer <jonas@gierer.xyz>
2024-12-28 16:12:45 +0100

    Install nvim smart-open file picker

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,9 +9,17 @@ return {
 				build = "make",
 			},
 			"nvim-telescope/telescope-ui-select.nvim",
+			{
+				"danielfalk/smart-open.nvim",
+				branch = "0.2.x",
+				dependencies = {
+					"kkharji/sqlite.lua",
+				},
+			},
 		},
 		config = function()
-			require("telescope").setup({
+			local telescope = require("telescope")
+			telescope.setup({
 				extensions = {
 					fzf = {},
 					["ui-select"] = {
@@ -19,11 +27,12 @@ return {
 					},
 				},
 			})
-			require("telescope").load_extension("fzf")
-			require("telescope").load_extension("ui-select")
+			telescope.load_extension("fzf")
+			telescope.load_extension("ui-select")
+			telescope.load_extension("smart_open")
 
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]iles" })
+			vim.keymap.set("n", "<leader>ff", telescope.extensions.smart_open.smart_open, { desc = "[f]ind [f]iles" })
 			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "[f]ind [r]ecent files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind by [g]rep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]uffers" })
