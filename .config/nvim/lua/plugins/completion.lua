db73e42bc210cff234a9c726cd792b02f5bd691b
Jonas Gierer <jonas@gierer.xyz>
2024-12-28 17:46:09 +0100

    Add better snippet plugins to nvim

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,13 +1,38 @@
 return {
 	{
 		"saghen/blink.cmp",
-		dependencies = "rafamadriz/friendly-snippets",
-
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			{
+				"L3MON4D3/LuaSnip",
+				version = "v2.*",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+					require("luasnip").filetype_extend("astro", { "typescript", "html" })
+				end,
+			},
+		},
 		version = "*",
-
 		opts = {
 			keymap = { preset = "default" },
 			signature = { enabled = true },
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
+			sources = {
+				default = { "lsp", "path", "luasnip", "buffer" },
+			},
 		},
 	},
 }
