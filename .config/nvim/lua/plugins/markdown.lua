a914c66201e075b61b11f12bb59f6c9c3f9ae5c1
Jonas Gierer <jonas@gierer.xyz>
2024-12-30 15:23:40 +0100

    Install nvim render-markdown plugin

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		{ "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+}
