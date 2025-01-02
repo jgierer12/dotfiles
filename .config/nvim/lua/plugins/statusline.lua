36022e441446dd63db759a840b7e305fa5201eaf
Jonas Gierer <jonas@gierer.xyz>
2025-01-02 12:36:09 +0100

    Improve nvim statusline

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -1,12 +1,30 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
 		config = function()
 			require("lualine").setup({
 				options = {
 					component_separators = "",
 					section_separators = "",
+				},
+				sections = {
+					lualine_b = {
+						{
+							"branch",
+							icon = "",
+						},
+						"diff",
+						"diagnostics",
+					},
+					lualine_c = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
 				},
 			})
 		end,
