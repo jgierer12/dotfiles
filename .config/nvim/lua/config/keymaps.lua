019b21dde7da8d652f3b96eb846d75e149c1aae9
Jonas Gierer <jonas@gierer.xyz>
2025-01-03 00:49:26 +0100

    Add more nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,18 @@ vim.keymap.set("n", "<leader>qa", "<cmd>qa<cr>", { desc = "[q]uit [a]ll" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
 
 vim.keymap.set("n", "gb", "<c-t>", { desc = "[g]o [b]ack" })
+
+vim.keymap.set("n", "<leader><leader>", "<c-6>", { desc = "Edit previous file" })
+
+-- Easier to reach start/end of line
+vim.keymap.set({ "n", "o", "v" }, "H", "^")
+vim.keymap.set({ "n", "o", "v" }, "L", "$")
+
+-- Re-select after indenting in visual mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Some of these require a terminal with Kitty Keyboard Protocol
+vim.keymap.set("i", "<c-cr>", "<esc>o", { desc = "Insert newline" })
+vim.keymap.set("i", "<c-bs>", "<c-w>", { desc = "Delete word before cursor" })
+vim.keymap.set("i", "<c-del>", "<c-o>dw", { desc = "Delete word after cursor" })
