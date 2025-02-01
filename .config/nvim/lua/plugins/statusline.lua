e835ca6970ff8b3c064e19575536912a850faa53
Jonas Gierer <jonas@gierer.xyz>
2025-02-01 20:42:38 +0900

    feat(nvim): Add rounded separators to statusline

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -8,7 +8,7 @@ return {
 			require("lualine").setup({
 				options = {
 					component_separators = "",
-					section_separators = "",
+					section_separators = { left = "", right = "" },
 				},
 				sections = {
 					lualine_b = {
