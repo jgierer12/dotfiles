da8ed229ce36f33290c341ac3040fe6f82a88809
Jonas Gierer <jonas@gierer.xyz>
2025-04-30 17:50:36 +0200

    chore(nvim): Move snippet plugins into separate file

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -2,15 +2,6 @@ return {
 	{
 		"saghen/blink.cmp",
 		dependencies = {
-			"rafamadriz/friendly-snippets",
-			{
-				"L3MON4D3/LuaSnip",
-				version = "v2.*",
-				config = function()
-					require("luasnip.loaders.from_vscode").lazy_load()
-					require("luasnip").filetype_extend("astro", { "typescript", "html" })
-				end,
-			},
 			"moyiz/blink-emoji.nvim",
 			{
 				"Kaiser-Yang/blink-cmp-dictionary",
