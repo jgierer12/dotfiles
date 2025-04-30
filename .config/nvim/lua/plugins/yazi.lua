2f8bc238c73510f6182917c21882a42d113c7273
Jonas Gierer <jonas@gierer.xyz>
2025-04-30 23:52:11 +0200

    Revert "feat(nvim/yazi): Manage yazi plugins in nvim"
    This reverts commit d88137471584f1d2704ed1511f633375049180c5.

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,47 +1,22 @@
 return {
-	{
-		"mikavilpas/yazi.nvim",
-		event = "VeryLazy",
-		keys = {
-			{
-				"<leader>e",
-				"<cmd>Yazi<cr>",
-				desc = "Open yazi at the current file",
-			},
-			{
-				"<leader>E",
-				"<cmd>Yazi cwd<cr>",
-				desc = "Open the file manager in nvim's working directory",
-			},
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>e",
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
 		},
-		opts = {
-			open_for_directories = true,
-			keymaps = {
-				show_help = "<f1>",
-			},
+		{
+			"<leader>E",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
 		},
 	},
-	{
-		"yazi-rs/flavors",
-		name = "yazi-rs-flavors",
-		lazy = true,
-		build = function(spec)
-			require("yazi.plugin").build_flavor(spec, {
-				sub_dir = "catppuccin-mocha.yazi",
-			})
-		end,
-	},
-	{
-		"yazi-rs/plugins",
-		name = "yazi-rs-plugins",
-		lazy = true,
-		build = function(spec)
-			require("yazi.plugin").build_plugin(spec, {
-				sub_dir = "git.yazi",
-			})
-			require("yazi.plugin").build_plugin(spec, {
-				sub_dir = "smart-enter.yazi",
-			})
-		end,
+	opts = {
+		open_for_directories = true,
+		keymaps = {
+			show_help = "<f1>",
+		},
 	},
 }
