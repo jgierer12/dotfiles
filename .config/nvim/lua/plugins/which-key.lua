953337439487ff79194fea2bd9954a37b714fa1c
Jonas Gierer <jonas@gierer.xyz>
2025-04-30 17:27:15 +0200

    feat(nvim): Change mini.surround keybinds
    They were conflicting with flash's keybinds

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -9,6 +9,7 @@ return {
 				{ "<leader>w", group = "[w]rite" },
 				{ "<leader>q", group = "[q]uit" },
 				{ "<leader>n", group = "[n]eovim config" },
+				{ "<leader>s", group = "[s]urround" },
 			},
 			preset = "helix",
 			delay = 0,
