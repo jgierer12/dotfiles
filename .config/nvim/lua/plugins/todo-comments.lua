afd802ace3e704af596965076b4533a989b8c1ba
Jonas Gierer <jonas@gierer.xyz>
2025-04-30 22:01:47 +0200

    feat(nvim): Add picker for todo comments

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -3,5 +3,18 @@ return {
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
+		specs = {
+			"folke/snacks.nvim",
+			keys = {
+				{
+					"<leader>ft",
+					function()
+						---@diagnostic disable-next-line: undefined-field
+						Snacks.picker.todo_comments()
+					end,
+					desc = "[f]ind [t]odos",
+				},
+			},
+		},
 	},
 }
