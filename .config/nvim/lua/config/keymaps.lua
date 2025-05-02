d05003c0d999391cf36d2d4669b0f1d99932adfb
Jonas Gierer <jonas@gierer.xyz>
2025-05-02 17:37:43 +0200

    feat(nvim): Add keymaps for folding to specific levels

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,3 +22,9 @@ vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("i", "<c-cr>", "<esc>o", { desc = "Insert newline" })
 vim.keymap.set("i", "<c-bs>", "<c-w>", { desc = "Delete word before cursor" })
 vim.keymap.set("i", "<c-del>", "<c-o>dw", { desc = "Delete word after cursor" })
+
+-- Folds
+vim.keymap.set("n", "z1", "zM", { desc = "Fold to level 1" })
+for i = 2, 9 do
+	vim.keymap.set("n", "z" .. i, "zM" .. i - 1 .. "zr", { desc = "Fold to level " .. i })
+end
