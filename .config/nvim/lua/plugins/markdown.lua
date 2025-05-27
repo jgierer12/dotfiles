80546db165e6887b6b64d2de90dfcec1eb21359e
Jonas Gierer <jonas@gierer.xyz>
2025-05-27 19:52:57 +0200

    feat(nvim): Add img-clip plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -42,4 +42,16 @@ return {
 		},
 		ft = "markdown",
 	},
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add options here
+			-- or leave it empty to use the default settings
+		},
+		keys = {
+			-- suggested keymap
+			{ "<leader>mp", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		},
+	},
 }
