7a901015cd0d389de492f1058812c92e075811b1
Jonas Gierer <jonas@gierer.xyz>
2025-11-15 16:36:23 +0100

    feat(nvim): Add biome formatter

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,3 +1,8 @@
+local formatters = {
+	lua = { "stylua" },
+	rust = { "rustfmt", lsp_format = "fallback" },
+}
+
 local prettier_filetypes = {
 	"javascript",
 	"javascriptreact",
@@ -15,13 +20,32 @@ local prettier_filetypes = {
 	"astro",
 }
 
-local formatters = {
-	lua = { "stylua" },
-	rust = { "rustfmt", lsp_format = "fallback" },
+local biome_filetypes = {
+	"javascript",
+	"javascriptreact",
+	"typescript",
+	"typescriptreact",
+	"svelte",
+	"css",
+	"html",
+	"json",
+	"jsonc",
+	"graphql",
+	"astro",
 }
 
+-- Add prettier & prettierd before biome
+-- Set stop_after_first so only the first valid formatter is used
+-- Together with require_cwd on the prettier formatter, this will use prettier
+-- if the project has a prettier config file, and otherwise biome
 for _, ft in ipairs(prettier_filetypes) do
-	formatters[ft] = { "prettierd", "prettier", stop_after_first = true }
+	formatters[ft] = { stop_after_first = true, "prettierd", "prettier" }
+end
+for _, ft in ipairs(biome_filetypes) do
+	if (not formatters[ft]) then
+		formatters[ft] = {}
+	end
+	table.insert(formatters[ft], "biome")
 end
 
 return {
@@ -30,6 +54,15 @@ return {
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
 		opts = {
+			formatters = {
+				-- Only use prettier/prettierd if project has a prettier config
+				prettierd = {
+					require_cwd = true,
+				},
+				prettier = {
+					require_cwd = true,
+				}
+			},
 			notify_on_error = false,
 			format_on_save = {
 				timeout_ms = 500,
