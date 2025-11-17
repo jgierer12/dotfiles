86e4a038ae080d0820e201c4f610ba18b6b5b3f7
Jonas Gierer <jonas@gierer.xyz>
2025-11-17 14:19:32 +0100

    feat(nvim): Tweak dashboard layout

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -62,6 +62,8 @@ return {
 					{ section = "header" },
 					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ section = "keys", padding = 1 },
+					{ section = "startup" },
 				},
 			},
 		},
